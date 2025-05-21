031991c88b1b3ad44d947bb145950339c2b885e8
NullClock <146401816+NullClock@users.noreply.github.com>
2025-05-21 18:39:11 -0400

    Update client.browser.js

--- browser/client.browser.js
+++ browser/client.browser.js
@@ -1,72 +1,59 @@
-// PushSocket.js (browser-ready)
-// Replace this with your secure ID generator or a placeholder
-import { generateSecureID } from './SGen.js'; // Make sure SGen.js is browser-friendly
+import { generateSecureID } from "./SGen.js";
 
 class PushSocket {
-  constructor(config, params, onConnect) {
+  constructor(config = {}, params = {}, onConnect = () => {}) {
     this._serverURL = "wss://ps-01.xapktech.xyz";
     this.config = config;
-    this.space = config?.space_id || "global";
-    this.connectedCallback = onConnect;
-    this.spacePassword = config?.password || "password";
+    this.space = config.space_id || "global";
+    this.spacePassword = config.password || "password";
     this.id = generateSecureID();
-    this.connected = false;
+    this.connectedCallback = onConnect;
     this.ws = null;
+    this.connected = false;
     this.params = params;
   }
 
-  observe(channel, callback) {
+  observe(channel = "GLOBAL", callback) {
     if (!this.ws) return;
     this.ws.addEventListener("message", (e) => {
       const d = JSON.parse(e.data);
-      channel = channel || "GLOBAL";
-
-      if (d.type !== "message") return;
-      if (d.channel !== channel) return;
-
-      callback(d);
+      if (d.type === "message" && d.channel === channel) callback(d);
     });
   }
 
-  send(data, channel) {
+  send(data, channel = "GLOBAL") {
     if (!this.ws) return;
     this.ws.send(JSON.stringify({
       type: "message",
-      channel: channel || "GLOBAL",
-      data: data,
+      channel,
+      data,
       id: this.id
     }));
   }
 
-  connect(space_id = this.config?.space_id || "global", space_password = this.config?.password || "password", params = this.params) {
+  connect(space_id = this.space, space_password = this.spacePassword, params = this.params) {
     this.ws = new WebSocket(this._serverURL);
     this.space = space_id;
     this.spacePassword = space_password;
 
     this.ws.onopen = () => {
-      this.ws.send(JSON.stringify({
+      this.ws?.send(JSON.stringify({
         type: "connect",
         id: this.id,
         space: this.space,
         password: this.spacePassword,
-        params: params
+        params
       }));
     };
 
     this.ws.onmessage = (e) => {
       const d = JSON.parse(e.data);
-
       if (d.type === "error") {
-        if (d.error === "invalid_password") {
-          throw new Error("PUSH_SOCKET_ERROR: INVALID PASSWORD.");
-        } else if (d.error === "invalid_space") {
-          throw new Error("PUSH_SOCKET_ERROR: INVALID SPACE.");
-        }
+        if (d.error === "invalid_password") throw new Error("PUSH_SOCKET_ERROR: INVALID PASSWORD.");
+        if (d.error === "invalid_space") throw new Error("PUSH_SOCKET_ERROR: INVALID SPACE.");
       } else if (d.type === "connect_success") {
         this.connected = true;
-        if (typeof this.connectedCallback === "function") {
-          this.connectedCallback();
-        }
+        this.connectedCallback();
       }
     };
   }
